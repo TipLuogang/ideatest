d4d28da75dad8343f31b17349eeda2c60cc1cb55
LG <1070130762@qq.com>
2018-03-21 14:14:00 +0800

    add user

---
+++ src/main/java/com/itheima/boot1/demo1/pojo/User2.java
@@ -0,0 +1,22 @@
+package com.itheima.boot1.demo1.pojo;
+
+//@description
+//   com.itheima.boot1.demo1.pojo
+//@author
+//    LG
+//@version
+//    1.0
+//@date
+//    2018/3/21
+
+public class User2 {
+    private  String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
